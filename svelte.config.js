5cc6b4a9db0dab6ee1ce8121ce71e6b2572c66f5
Zirui Project <88216794+ziruiproject@users.noreply.github.com>
2023-12-28 21:09:11 +0700

    added tailwindcss

--- svelte.config.js
+++ svelte.config.js
@@ -1,7 +1,10 @@
-import { vitePreprocess } from '@sveltejs/vite-plugin-svelte'
-
-export default {
-  // Consult https://svelte.dev/docs#compile-time-svelte-preprocess
-  // for more information about preprocessors
-  preprocess: vitePreprocess(),
-}
+import adapter from '@sveltejs/adapter-auto';
+import { vitePreprocess } from '@sveltejs/vite-plugin-svelte';
+/** @type {import('@sveltejs/kit').Config} */
+const config = {
+  kit: {
+    adapter: adapter()
+  },
+  preprocess: vitePreprocess()
+};
+export default config;
