2bb5a4f236c05345c8ba682b1d473334fb0de408
Zirui Project <88216794+ziruiproject@users.noreply.github.com>
2023-12-29 07:17:30 +0700

    added style

--- src/App.svelte
+++ src/App.svelte
@@ -38,8 +38,10 @@
       <Youtube size="32"></Youtube>
       <Twitter size="32"></Twitter>
     </div>
-    <div>
-      <button class="flex items-center px-16 py-4 bg-white rounded-[10px]">
+    <div class=" place-content-center place-items-center grid gap-5">
+      <button
+        class="text-dark-gray flex items-center px-16 py-4 bg-white rounded-[10px]"
+      >
         <span class="pr-2 -mb-1 text-2xl font-medium">Contact Me</span>
         <svg
           xmlns="http://www.w3.org/2000/svg"
@@ -63,8 +65,10 @@
         </svg>
       </button>
       <span class="text-light-gray pl-2 text-xl font-normal"> or </span>
-      <button class="flex items-center px-16 py-4 bg-white rounded-[10px]">
-        <span class="pr-2 -mb-1 text-2xl font-medium">Contact Me</span>
+      <button
+        class="flex items-center px-16 py-4 text-white-text bg-dark-gray rounded-[10px]"
+      >
+        <span class="pr-2 -mb-1 text-2xl font-medium">Copy Email</span>
         <svg
           xmlns="http://www.w3.org/2000/svg"
           width="24"
@@ -73,16 +77,18 @@
           fill="none"
         >
           <path
-            d="M7 9L12 12.5L17 9"
-            stroke="#201F20"
+            d="M19.4 20H9.6C9.26863 20 9 19.7314 9 19.4V9.6C9 9.26863 9.26863 9 9.6 9H19.4C19.7314 9 20 9.26863 20 9.6V19.4C20 19.7314 19.7314 20 19.4 20Z"
+            stroke="#D9D9D9"
             stroke-width="1.5"
             stroke-linecap="round"
             stroke-linejoin="round"
           />
           <path
-            d="M2 17V7C2 5.89543 2.89543 5 4 5H20C21.1046 5 22 5.89543 22 7V17C22 18.1046 21.1046 19 20 19H4C2.89543 19 2 18.1046 2 17Z"
-            stroke="#201F20"
+            d="M15 9V4.6C15 4.26863 14.7314 4 14.4 4H4.6C4.26863 4 4 4.26863 4 4.6V14.4C4 14.7314 4.26863 15 4.6 15H9"
+            stroke="#D9D9D9"
             stroke-width="1.5"
+            stroke-linecap="round"
+            stroke-linejoin="round"
           />
         </svg>
       </button>
